42fe70eb5a023ca89dfabf2577b87bd9d3d8d230
Bogdan Onikiienko <onikiienko@gmail.com>
2014-10-23 18:32:55 +0300

    express setup + hash

---
+++ client/js/script.js
@@ -0,0 +1,60 @@
+var room;
+var socket = io();
+var gamers = {};
+var resultsOfAGame;
+
+
+if(window.location.hash) {
+  // Fragment exists
+  joinRoom(window.location.hash.substr(1));
+  console.log(window.location.hash.substr(1));
+} else {
+}
+
+function createRoom(){
+	room = '/' + makeid();
+	socket.emit('create room', room);
+	socket = io(room);
+	socketInit();
+	window.location.hash = room;
+}	
+
+function joinRoom(room){
+	socket = io(room);
+	socketInit()
+}
+
+function socketInit(){
+	window.login = makeid();
+	socket.emit('add gamer', login);
+	socket.on('update game', function(game){
+		gamers = game;
+		$(".table").empty();
+		_.each(gamers, function(gamer){
+			$(".table").append("<div class='" + gamer.login + " gamerCard'>" + gamer.login + "</div>");
+			console.log(gamer);
+		})
+	});
+	socket.on('borders are', function(result){
+		resultsOfAGame = result;
+		console.log(resultsOfAGame);
+	});
+}
+
+function vote(){
+	var points = $('.points').val();
+	socket.emit('gamer vote', login, points);
+}
+
+function makeid(){
+	var text = "",
+		possible = "0123456789";
+
+	for( var i=0; i < 5; i++ )
+	    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
+}
+function openCards(){
+	socket.emit('choose borders');
+}
