6fa707f0520e2bda7aeaac71a0de2e5052cf635e
Bogdan Onikiienko <onikiienko@gmail.com>
2015-02-16 20:32:18 +0200

    github oauth

--- client/js/views/loginView.js
+++ client/js/views/loginView.js
@@ -5,7 +5,8 @@ var LoginView = Backbone.View.extend({
 		this.$el.css('display', 'table');
 	},
 	events: {
-		"click .loginBtn" : "loginSubmit"
+		"click .loginBtn" : "loginSubmit",
+		"click .loginWithGithybBtn" : "loginWithGithub"
 	},
 	loginSubmit: function(){
 		var login = $('.loginInput').val();
@@ -18,6 +19,31 @@ var LoginView = Backbone.View.extend({
 		}else{
 			createOrJoinView.render();
 		}
+	},
+	loginWithGithub: function(){
+		OAuth.initialize('DR4zizVjOy_1ZXdtlmn0GBLoTcA');
+		OAuth.popup('github')
+		.done(function(result) {
+		  result.me()
+			.done(function(me){
+				tableModule.set({'login' : me.name});
+				if ($('.loginCheckBox').prop('checked')){
+					document.cookie = 'login=' + me.name;
+				}
+				if(window.location.hash.substring(2)){
+					createOrJoinView.joinRoom(window.location.hash.substring(1));
+				}else{
+					createOrJoinView.render();
+				}
+			})
+			.fail(function (err) {
+				console.log(err);
+			});
+		})
+		.fail(function (err) {
+		  //handle error with err
+		  console.log(err);
+		});
 	}
 });
 
