417e8c5030364f74a3a99d9108e913d2891f5f6b
Bogdan Onikiienko <onikiienko@gmail.com>
2015-05-07 22:40:43 +0300

    readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,6 @@ module.exports = function(grunt) {
 	// Default task(s).
 	grunt.registerTask('default', ['devbuild', 'watch']);
 	grunt.registerTask('devbuild', ['remove', 'babel', 'less', 'copy', 'jshint', 'test']);
-	grunt.registerTask('build', ['devbuild', 'uglify']);
+	grunt.registerTask('build', ['remove', 'babel', 'less', 'copy', 'jshint', 'uglify']);
 	grunt.registerTask('test', ['karma']);
 };
