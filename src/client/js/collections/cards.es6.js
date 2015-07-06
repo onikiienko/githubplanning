b85823bfc7d48520ac3927b38978a774067a298b
Bogdan Onikiienko <onikiienko@gmail.com>
2015-07-06 21:40:31 +0300

    fucking mistake

--- src/client/js/collections/cards.es6.js
+++ src/client/js/collections/cards.es6.js
@@ -6,15 +6,15 @@ define('collections/cards', [
 	], function(CardModel, Backbone, _){
 		var Cards = Backbone.Collection.extend({
 			addCard: function(model){
-				if(this.filter(function (nextCard) { return _.isEqual(nextCard.get("cotributor"), model.cotributor);})){
-					this.remove(this.filter(function (nextCard) { return _.isEqual(nextCard.get("cotributor"), model.cotributor);}));
+				if(this.filter(function (nextCard) { return _.isEqual(nextCard.get("contributor"), model.contributor);})){
+					this.remove(this.filter(function (nextCard) { return _.isEqual(nextCard.get("contributor"), model.contributor);}));
 					this.add(new CardModel(model));
 				}else{
 					this.add(new CardModel(model));
 				}
 			},
 			removeCard: function(model){
-				this.remove(this.filter(function (nextCard) { return _.isEqual(nextCard.get("cotributor"), model.cotributor);}));
+				this.remove(this.filter(function (nextCard) { return _.isEqual(nextCard.get("contributor"), model.contributor);}));
 			}
 		});
 
