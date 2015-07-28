e266c1e615fc592df4b12cbbc973650a40236add
RoYa <roya3000@ukr.net>
2015-07-28 15:46:25 +0300

    add flip card

--- src/client/js/views/card.es6.js
+++ src/client/js/views/card.es6.js
@@ -9,51 +9,51 @@ define('views/card', [
 ], function(cardTemplate, CardModel, io, Backbone, _, appData) {
 	let PlaygroundCard = Backbone.View.extend({
 		el: '.cards',
-		
+
 		template : _.template(cardTemplate),
-		
+
 		events: {
 			'click .card__btn' : 'removeCardFromCollection'
 		},
-		
+
 		initialize: function(){
 			this.listenTo(this.collection, "add", function(card){
 				this.model = card;
 				this.addCard();
 			});
-		
+
 			this.listenTo(this.collection, "remove", function(card){
 				this.model = card;
 				this.removeCardFromLayout();
 			});
 		},
-		
+
 		addCard: function(){
 			$(this.el).append(this.template({model: this.model.toJSON(), headerModel: appData.headerModel}));
 		},
-		
+
 		removeCardFromLayout: function(){
 			let avatar = this.model.get('contributor').avatar;
 			let userBlock = $(this.el).find('[src="' + avatar + '"]');
-			let card = $(userBlock).closest('.card');
+			let card = $(userBlock).closest('.flip-container');
 
 			$(card).remove();
 		},
-		
+
 		removeCardFromCollection: function(e){
-			let value = $($($(e.target).closest(".card")).find(".card__rate")).attr("data");
-			let content = $($($(e.target).closest(".card")).find(".card__rate")).text();
-			
+			let value = $($($(e.target).closest(".flip-container")).find(".card__rate")).attr("data");
+			let content = $($($(e.target).closest(".flip-container")).find(".card__rate")).text();
+
 			let playerObject = {
         		avatar: appData.headerModel.get('avatar'),
         		name: appData.headerModel.get('name')
 			};
-			
-			let model = new CardModel({ 
-				contributor: playerObject, 
-				card: { 
-					value: value, 
-					content: content 
+
+			let model = new CardModel({
+				contributor: playerObject,
+				card: {
+					value: value,
+					content: content
 				}
 			});
 
