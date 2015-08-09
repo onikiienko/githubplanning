2f5a650147e99fd13c22a7b8ea7e9d2f9815e6e3
RoYa <roya3000@ukr.net>
2015-08-09 13:48:43 +0300

    add click handler for project list

--- src/client/js/views/create.es6.js
+++ src/client/js/views/create.es6.js
@@ -8,26 +8,38 @@ define('views/create', [
 ], function(appData, createTemplate, Backbone, $, _) {
   let Create = Backbone.View.extend({
     el: '.main-content',
-    
+
     events: {
       "click .enterRoomBtn" : "createRoom",
-      "click .select__title" : "openRoomNameList",
-      "click .select__item" : "chooseProject"
+      "click .select__item" : "chooseProject",
+      "click" : "handleRoomList"
     },
-    
+
     template: _.template(createTemplate),
-    
+
     initialize: function(options){
         this.router = options.router;
 
         _.bindAll(this, 'render');
         this.model.bind('change', this.render);
+        //handle esc key
+        _.bindAll(this, 'closeRoomNameList');
+        $(document).on('keydown', this.closeRoomNameList);
     },
-    
+
     render: function(){
         $(this.el).html(this.template(this.model.toJSON()));
     },
-    
+
+    handleRoomList: function (e) {
+        if ( $(e.target).attr("class") !== "select__title" ) {
+            $('.select__items').hide();
+            return;
+          }
+
+          $('.select__items').toggle();
+    },
+
     createRoom: function(){
       let roomName = $.trim($('.select__title').html());
       let roomUrl = '#room/' + roomName.replace('/', ';)');
@@ -42,14 +54,10 @@ define('views/create', [
       appData.headerModel.set('projectName', roomName);
       appData.headerModel.set('projectUrl', projectUrl);
       appData.headerModel.set('currencyType', $('input:checked').attr('data'));
- 
+
       this.router.navigate(roomUrl, {trigger: true});
     },
-    
-    openRoomNameList: function(){
-      $('.select__items').show();
-    },
-    
+
     chooseProject: function(target){
       let choosenProject = $(target.target)
           .closest('.select__item')
@@ -60,7 +68,13 @@ define('views/create', [
       $('.select__title').html(choosenProject);
       $('.select__items').hide();
 
-    }
+    },
+
+    closeRoomNameList: function(e) {
+      if (e.which === 27) {
+        $('.select__items').hide();
+      }
+    },
   });
 
   return Create;
